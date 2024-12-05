00d7f417032e08955a396701633eda2b4b53cb5c
polaris <61411085+polaris428@users.noreply.github.com>
2024-12-05 14:58:26 +0900

    Initial commit

---
+++ WavyCircleView/build.gradle.kts
@@ -0,0 +1,73 @@
+plugins {
+    id("com.android.library")
+    id("org.jetbrains.kotlin.android")
+    id("maven-publish")
+}
+
+android {
+    namespace = "com.polaris.wavycircleview"
+    compileSdk = 34
+
+    defaultConfig {
+        minSdk = 28
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_11
+        targetCompatibility = JavaVersion.VERSION_11
+    }
+    kotlinOptions {
+        jvmTarget = "11"
+    }
+}
+
+dependencies {
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.appcompat)
+    implementation(libs.material)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+}
+
+afterEvaluate {
+    publishing {
+        publications {
+            // Creates a Maven publication called "release".
+            create("release", MavenPublication::class.java) {
+                // Applies the component for the release build variant.
+                from(components["release"])
+
+                // You can then customize attributes of the publication as shown below.
+                groupId = "com.github.polaris428"
+                artifactId = "WavyCircleView"
+                version = "0.0.1"
+            }
+
+            // Creates a Maven publication called "debug".
+            create("debug", MavenPublication::class.java) {
+                // Applies the component for the debug build variant.
+                from(components["debug"])
+
+                groupId = "com.github.polaris428"
+                artifactId = "WavyCircleView"
+                version = "0.0.1"
+            }
+        }
+    }
+}
+
+group="com.github.polaris428"
---
+++ build.gradle.kts
@@ -0,0 +1,14 @@
+buildscript {
+    dependencies {
+        classpath("com.github.dcendents:android-maven-gradle-plugin:2.1")
+
+    }
+}
+
+// Top-level build file where you can add configuration options common to all sub-projects/modules.
+plugins {
+    id("com.android.application") version ("8.7.2")  apply(false)
+    id("org.jetbrains.kotlin.android") version( "1.9.24") apply(false)
+
+}
+
